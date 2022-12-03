a2f5811a30c71f895b8945a32f72dd97952b851f
Gyula Samu <gyula.samu@furukawaelectric.com>
2022-12-03 19:15:31 +0100

    harmadik masodik

---
+++ 3/cucc2.hs
@@ -0,0 +1,27 @@
+import Data.List
+
+main = do  
+    contents <- getContents  
+    let 
+        fileLines = lines contents
+        groupList = groups fileLines
+        sumPrior = sum $ getPrior $ map sects groupList
+    putStr $  show sumPrior ++ "\n"
+
+groups :: [String] -> [(String, String, String)]
+groups (x:y:z:lines) = (x, y, z) : (groups lines)
+groups _             = []
+
+sects :: (String, String, String) -> Char
+sects (x,y,z) = (nub $ intersect x $ intersect y z) !! 0
+
+priorities :: [(Char,Int)]
+priorities = zip (['a'..'z'] ++ ['A'..'Z']) [1..]
+
+getVal :: [(Char,Int)] -> Char -> Int
+getVal ((key,val):xs) x
+    | x == key  = val
+    | otherwise = getVal xs x
+
+getPrior :: [Char] -> [Int]
+getPrior = map (getVal priorities)
