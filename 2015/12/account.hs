6281083ac8f156d780ffa25389c7e213eeab974d
Gyula Samu <gyula.samu@furukawaelectric.com>
2022-12-05 23:12:40 +0100

    2015 12.

--- 2015/12/account.hs
+++ 2015/12/account.hs
@@ -1,9 +1,18 @@
 import Data.Char
-import System.IO.Unsafe
+import System.IO
 
-contents = unsafePerformIO . readFile $ "data.txt"
+main = do
+    handle <- openFile "data.txt" ReadMode
+    contents <- hGetContents handle
+    let
+        res1 = addNums contents
+        res2 = addNums $ reduceRed contents
+    putStr $ result [res1,res2]
+    hClose handle
 
---fileLines = lines contents
+                                                                                                                                 
+result :: Show a => [a] -> String
+result xs = unlines $ map show xs
 
 getNum :: String -> Maybe (Int,String)
 getNum str  = case (getNumStr str) of
@@ -13,7 +22,7 @@ getNum str  = case (getNumStr str) of
 getNumStr :: String -> (String,String)
 getNumStr "" = ("","")
 getNumStr (x:y:xs) 
-    | x == '-' && isDigit y = (x : y : (fst (getNumStr xs)), (snd (getNumStr xs)))
+    | x == '-' && isDigit y = (x : (fst (getNumStr (y:xs))), (snd (getNumStr (y:xs))))
     | isDigit x && (not . isDigit) y = ([x],(y:xs))
 getNumStr (x:xs) 
     | isDigit x = (x : (fst (getNumStr xs)), (snd (getNumStr xs)))
@@ -26,3 +35,60 @@ addNums str = case (getNum str) of
 
 showNum :: Maybe (Int,String) -> Int
 showNum (Just (x,str)) = x
+
+containsRed :: String -> Bool
+containsRed (x:y:"") = False
+containsRed (x:y:z:xs) = case (x:y:z:"") of
+    "red" -> True
+    other -> containsRed (y:z:xs)
+
+getBeforeRed :: String -> String
+getBeforeRed (x:y:"") = x:y:""
+getBeforeRed (x:y:z:xs) = case (x:y:z:"") of
+    "red" -> ""
+    other -> x : getBeforeRed (y:z:xs)
+
+getAfterRed :: String -> String
+getAfterRed (x:y:"") = x:y:""
+getAfterRed (x:y:z:xs) = case (x:y:z:"") of
+    "red" -> xs
+    other -> getAfterRed (y:z:xs)
+
+getAfterBracket :: String -> String
+getAfterBracket str
+    | length scr1 < length scr2 = drop ((length scr1)-1) str
+    | otherwise = str
+    where
+        scr1 = firstUnpairedCurly  "forward" str 0
+        scr2 = firstUnpairedSquare "forward" str 0
+
+getBeforeBracket :: String -> String
+getBeforeBracket str 
+    | length scr1 < length scr2 = reverse (drop ((length scr1)-1) (reverse str))
+    | otherwise = str
+    where
+        scr1 = firstUnpairedCurly  "backward" (reverse str) 0
+        scr2 = firstUnpairedSquare "backward" (reverse str) 0
+
+firstUnpairedCurly :: String -> String -> Int -> String
+firstUnpairedCurly "forward"  xs   1  = ""
+firstUnpairedCurly "backward" xs (-1) = ""
+firstUnpairedCurly dir ('}':xs) n = '}':firstUnpairedCurly dir xs (n+1)
+firstUnpairedCurly dir ('{':xs) n = '{':firstUnpairedCurly dir xs (n-1)
+firstUnpairedCurly dir (x:xs)   n = x : firstUnpairedCurly dir xs n
+firstUnpairedCurly "forward"  "" n = ""
+firstUnpairedCurly "backward" "" n = ""
+
+firstUnpairedSquare :: String -> String -> Int -> String
+firstUnpairedSquare "forward"  xs   1  = ""
+firstUnpairedSquare "backward" xs (-1) = ""
+firstUnpairedSquare dir (']':xs) n = ']':firstUnpairedSquare dir xs (n+1)
+firstUnpairedSquare dir ('[':xs) n = '[':firstUnpairedSquare dir xs (n-1)
+firstUnpairedSquare dir (x:xs)   n = x : firstUnpairedSquare dir xs n
+firstUnpairedSquare "forward"  "" n = ""
+firstUnpairedSquare "backward" "" n = ""
+
+reduceRed :: String -> String
+reduceRed str = case (containsRed str) of
+    True -> reduceRed ( getBeforeBracket (getBeforeRed str) ++ ( getAfterBracket (getAfterRed str)))
+    False -> str
