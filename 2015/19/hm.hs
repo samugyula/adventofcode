57a1ef494d2ef8e1d3b3b93dd78ad60e0e2c0980
Gyula Samu <gyula.samu@furukawaelectric.com>
2022-12-10 01:46:41 +0100

    2015 19. update final

--- 2015/19/hm.hs
+++
@@ -1,9 +0,0 @@
-import Data.Set as Set
--- Taken From Yi
-ordNub :: (Ord a) => [a] -> [a]
-ordNub l = go Set.empty l
-  where
-    go _ []     = []
-    go s (x:xs) = if x `Set.member` s then go s xs
-                                      else x : go (Set.insert x s) xs
-
